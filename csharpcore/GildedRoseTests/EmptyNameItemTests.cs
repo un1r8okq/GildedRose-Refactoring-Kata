aaf97e3433aef5d115a39a567f83853e4c080ac9
Will Verland <will@ver.land>
2021-07-28 17:00:59 +1200

    Refactor empty name tests

--- csharpcore/GildedRoseTests/EmptyNameItemTests.cs
+++ csharpcore/GildedRoseTests/EmptyNameItemTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using GildedRoseKata;
 using System.Linq;
+using System.Diagnostics;
 
 namespace GildedRoseTests
 {
@@ -13,94 +14,113 @@ namespace GildedRoseTests
         [InlineData(0, -1)]
         [InlineData(-1, -2)]
         [InlineData(-10, -11)]
-        public void UpdateQuality_TwoItems_FirstItemSellInDecrements(
-            int initialSellIn,
-            int expectedSellIn)
+        public void WhenOneDayPasses_TheSellInValueDecreasesByOne(
+            int initial,
+            int expected)
         {
             var items = new List<Item>
             {
-                new Item { SellIn = initialSellIn },
-                new Item(),
+                new Item { SellIn = initial },
             };
             var sut = new GildedRose(items);
 
             sut.UpdateQuality();
 
-            Assert.Equal(expectedSellIn, items.First().SellIn);
+            Assert.Equal(expected, items.First().SellIn);
+        }
+
+        [Theory]
+        [InlineData(10, 8)]
+        [InlineData(3, 1)]
+        [InlineData(2, 0)]
+        [InlineData(1, -1)]
+        [InlineData(-1, -3)]
+        [InlineData(-10, -12)]
+        public void WhenTwoDaysPass_TheSellInValueDecreasesByTwo(
+            int initial,
+            int expected)
+        {
+            var items = new List<Item>
+            {
+                new Item { SellIn = initial },
+            };
+            var sut = new GildedRose(items);
+
+            sut.UpdateQuality();
+            sut.UpdateQuality();
+
+            Assert.Equal(expected, items.First().SellIn);
         }
 
         [Theory]
         [InlineData(10, 9)]
         [InlineData(1, 0)]
-        [InlineData(0, -1)]
-        [InlineData(-1, -2)]
-        [InlineData(-10, -11)]
-        public void UpdateQuality_TwoItems_SecondItemSellInDecrements(
-            int initialSellIn,
-            int expectedSellIn)
-        {
-            var items = new List<Item>
-            {
-                new Item(),
-                new Item { SellIn = initialSellIn },
-            };
-            var sut = new GildedRose(items);
-
-            sut.UpdateQuality();
-
-            Assert.Equal(expectedSellIn, items[1].SellIn);
-        }
-
-        [Theory]
-        [InlineData(2, 1)]
-        [InlineData(1, 0)]
-        public void UpdateQuality_SellInIsOne_QualityDecrements(
-            int initialQuality,
-            int expectedQuality)
+        public void WhenSellInIsOne_AndOneDayPasses_QualityDecreasesByOne(int initial, int expected)
         {
             var items = new List<Item>
             {
                 new Item
                 {
                     SellIn = 1,
-                    Quality = initialQuality,
+                    Quality = initial,
                 },
             };
             var sut = new GildedRose(items);
 
             sut.UpdateQuality();
 
-            Assert.Equal(expectedQuality, items.First().Quality);
+            Assert.Equal(expected, items.First().Quality);
         }
 
         [Theory]
-        [InlineData(0, 2, 0)]
-        [InlineData(-1, 2, 0)]
-        public void UpdateQuality_SellInLessThanOne_QualityDecreasesByTwo(
-            int initialSellIn,
-            int initialQuality,
-            int expectedQuality)
+        [InlineData(10, 8)]
+        [InlineData(3, 1)]
+        [InlineData(2, 0)]
+        public void WhenSellInIsZero_AndOneDayPasses_QualityDecreasesByTwo(int initial, int expected)
         {
             var items = new List<Item>
             {
                 new Item
                 {
-                    SellIn = initialSellIn,
-                    Quality = initialQuality,
+                    SellIn = 0,
+                    Quality = initial,
                 },
             };
             var sut = new GildedRose(items);
 
             sut.UpdateQuality();
 
-            Assert.Equal(expectedQuality, items.First().Quality);
+            Assert.Equal(expected, items.First().Quality);
         }
 
         [Theory]
+        [InlineData(10, 8)]
+        [InlineData(3, 1)]
+        [InlineData(2, 0)]
+        public void WhenSellInIsNegativeOne_AndOneDayPasses_QualityDecreasesByTwo(int initial, int expected)
+        {
+            var items = new List<Item>
+            {
+                new Item
+                {
+                    SellIn = -1,
+                    Quality = initial,
+                },
+            };
+            var sut = new GildedRose(items);
+
+            sut.UpdateQuality();
+
+            Assert.Equal(expected, items.First().Quality);
+        }
+
+        [Theory]
+        [InlineData(10)]
         [InlineData(1)]
         [InlineData(0)]
         [InlineData(-1)]
-        public void UpdateQuality_QualityIsZero_QualityDoesNotChange(int sellIn)
+        [InlineData(-10)]
+        public void WhenQualityIsZero_AndOneDayPasses_QualityRemainsZero(int sellIn)
         {
             var items = new List<Item>
             {
@@ -115,6 +135,21 @@ namespace GildedRoseTests
             sut.UpdateQuality();
 
             Assert.Equal(0, items.First().Quality);
+        }
+
+        [Fact]
+        public void When100000DaysPass_ItTakesLessThan200Ms()
+        {
+            var items = new List<Item>();
+            for (var i = 0; i < 100000; i++) items.Add(new Item());
+            var sut = new GildedRose(items);
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            sut.UpdateQuality();
+            stopwatch.Stop();
+
+            Assert.InRange(stopwatch.ElapsedMilliseconds, 0, 200);
         }
     }
 }
