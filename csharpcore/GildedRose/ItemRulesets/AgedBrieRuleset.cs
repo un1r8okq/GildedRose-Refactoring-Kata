0c522711797ebaa98b1af55d4299219fa9072708
Will Verland <will@ver.land>
2021-08-01 11:21:39 +1200

    Implement AgedBrieRuleset

--- csharpcore/GildedRose/ItemRulesets/AgedBrieRuleset.cs
+++ csharpcore/GildedRose/ItemRulesets/AgedBrieRuleset.cs
@@ -1,9 +1,36 @@
-﻿using System;
-
-namespace GildedRoseKata
+﻿namespace GildedRoseKata
 {
     public class AgedBrieRuleset : ItemRuleset
     {
-        public Item UpdateItem(Item item) => throw new NotImplementedException();
+        public Item UpdateItem(Item item) =>
+               new Item
+               {
+                   Name = item.Name,
+                   Quality = CalculateQuality(item),
+                   SellIn = CalculateSellIn(item),
+               };
+
+        private int CalculateQuality(Item item)
+        {
+            var updatedQuality = item.Quality;
+
+            if (item.SellIn > 0)
+            {
+                updatedQuality += 1;
+            }
+            else
+            {
+                updatedQuality += 2;
+            }
+
+            if (updatedQuality > 50)
+            {
+                updatedQuality = 50;
+            }
+
+            return updatedQuality;
+        }
+
+        private int CalculateSellIn(Item item) => item.SellIn - 1;
     }
 }
---
+++ csharpcore/GildedRoseTests/ItemRulesets/AgedBrieRulesetTests.cs
@@ -0,0 +1,128 @@
+﻿using GildedRoseKata;
+using System.Diagnostics;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class AgedBrieRulesetTests
+    {
+        [Theory]
+        [InlineData(10, 11)]
+        [InlineData(0, 1)]
+        public void WhenSellInIsOne_AndItemIsUpdated_QualityIncreasesByOne(
+            int initial,
+            int expected)
+        {
+            var item = new Item
+                {
+                    Name = "Aged Brie",
+                    Quality = initial,
+                    SellIn = 1,
+                };
+            var sut = new AgedBrieRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(expected, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(10, 12)]
+        [InlineData(0, 2)]
+        public void WhenSellInIsZero_AndItemIsUpdated_QualityIncreasesByTwo(
+            int initial,
+            int expected)
+        {
+            var item = new Item
+            {
+                Name = "Aged Brie",
+                Quality = initial,
+                SellIn = 0,
+            };
+            var sut = new AgedBrieRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(expected, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(10, 12)]
+        [InlineData(0, 2)]
+        public void WhenSellInIsNegativeOne_AndItemIsUpdated_QualityIncreasesByTwo(
+            int initial,
+            int expected)
+        {
+            var item = new Item
+            {
+                Name = "Aged Brie",
+                Quality = initial,
+                SellIn = -1,
+            };
+            var sut = new AgedBrieRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(expected, newItem.Quality);
+        }
+
+        [Fact]
+        public void WhenSellInIsOne_AndQualityIsFifty_AndItemIsUpdated_QualityDoesNotChange()
+        {
+            var item = new Item
+            {
+                Name = "Aged Brie",
+                Quality = 50,
+                SellIn = 1,
+            };
+            var sut = new AgedBrieRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(50, newItem.Quality);
+        }
+
+        [Fact]
+        public void WhenSellInIsZero_AndQualityIsFifty_AndItemIsUpdated_QualityDoesNotChange()
+        {
+            var item = new Item
+            {
+                Name = "Aged Brie",
+                Quality = 50,
+                SellIn = 0,
+            };
+            var sut = new AgedBrieRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(50, newItem.Quality);
+        }
+
+        [Fact]
+        public void WhenItemIsUpdated_NewObjectIsReturned()
+        {
+            var item = new Item();
+            var sut = new AgedBrieRuleset();
+
+            var newItem = sut.UpdateItem(new Item { Name = "Aged Brie" });
+
+            Assert.NotSame(item, newItem);
+        }
+
+        [Fact]
+        public void When100000ItemsAreUpdated_ItTakesLessThan200Ms()
+        {
+            var sut = new AgedBrieRuleset();
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (var i = 0; i < 100000; i++)
+            {
+                sut.UpdateItem(new Item { Name = "Aged Brie" });
+            }
+            stopwatch.Stop();
+
+            Assert.InRange(stopwatch.ElapsedMilliseconds, 0, 200);
+        }
+    }
+}
