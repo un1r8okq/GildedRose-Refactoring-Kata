3e9e896d08bc9be72434421d93490dd6ba636749
Will Verland <will@ver.land>
2021-08-01 11:52:57 +1200

    Implement BackstagePassesRuleset

--- csharpcore/GildedRose/ItemRulesets/BackstagePassesRuleset.cs
+++ csharpcore/GildedRose/ItemRulesets/BackstagePassesRuleset.cs
@@ -4,6 +4,43 @@ namespace GildedRoseKata
 {
     public class BackstagePassesRuleset : ItemRuleset
     {
-        public Item UpdateItem(Item item) => throw new NotImplementedException();
+        public Item UpdateItem(Item item) =>
+               new Item
+               {
+                   Name = item.Name,
+                   Quality = CalculateQuality(item),
+                   SellIn = CalculateSellIn(item),
+               };
+
+        private int CalculateQuality(Item item)
+        {
+            var updatedQuality = item.Quality;
+
+            if (item.SellIn > 10)
+            {
+                updatedQuality += 1;
+            }
+            else if (item.SellIn <= 10 && item.SellIn > 5)
+            {
+                updatedQuality += 2;
+            }
+            else if (item.SellIn <= 5 && item.SellIn > 0)
+            {
+                updatedQuality += 3;
+            }
+            else
+            {
+                updatedQuality = 0;
+            }
+
+            if (updatedQuality > 50)
+            {
+                updatedQuality = 50;
+            }
+
+            return updatedQuality;
+        }
+
+        private int CalculateSellIn(Item item) => item.SellIn - 1;
     }
 }
---
+++ csharpcore/GildedRoseTests/ItemRulesets/BackstagePassesRulesetTests.cs
@@ -0,0 +1,190 @@
+﻿using GildedRoseKata;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class BackstagePassesRulesetTests
+    {
+        [Theory]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void WhenSellInIsBetweenSixAndTen_AndItemIsUpdated_QualityIncreasesFromOneToThree(
+            int sellIn)
+        {
+            var item =  new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = 1,
+                SellIn = sellIn,
+            };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(3, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(48, 50)]
+        [InlineData(10, 12)]
+        [InlineData(1, 3)]
+        [InlineData(0, 2)]
+        public void WhenSellInIsTen_AndItemIsUpdated_QualityIncreasesByTwo(
+            int initial,
+            int expected)
+        {
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = initial,
+                SellIn = 10,
+            };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(expected, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(48)]
+        [InlineData(49)]
+        [InlineData(50)]
+        public void WhenSellInIsTen_AndQualityIsCloseToFifty_AndItemIsUpdated_QualityDoesNotPassFifty(
+            int quality)
+        {
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = quality,
+                SellIn = 10,
+            };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(50, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(4)]
+        [InlineData(3)]
+        [InlineData(2)]
+        [InlineData(1)]
+        public void WhenSellInIsBetweenOneAndFive_AndItemIsUpdated_QualityIncreasesFromOneToFour(
+            int sellIn)
+        {
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = 1,
+                SellIn = sellIn,
+            };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(4, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(47, 50)]
+        [InlineData(10, 13)]
+        [InlineData(1, 4)]
+        [InlineData(0, 3)]
+        public void WhenSellInIsFive_AndItemIsUpdated_QualityIncreasesByThree(
+            int initial,
+            int expected)
+        {
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = initial,
+                SellIn = 5,
+            };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(expected, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(47)]
+        [InlineData(48)]
+        [InlineData(49)]
+        [InlineData(50)]
+        public void WhenSellInIsFive_AndQualityIsCloseToFifty_AndItemIsUpdated_QualityDoesNotPassFifty(
+            int quality)
+        {
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = quality,
+                SellIn = 5,
+            };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(50, newItem.Quality);
+        }
+
+        [Theory]
+        [InlineData(50)]
+        [InlineData(10)]
+        [InlineData(1)]
+        [InlineData(0)]
+        public void WhenSellInIsZero_AndItemIsUpdated_QualityBecomesZero(
+            int quality)
+        {
+
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = quality,
+                SellIn = 0,
+            };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(0, newItem.Quality);
+        }
+
+        [Fact]
+        public void WhenItemIsUpdated_NewObjectIsReturned()
+        {
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert" };
+            var sut = new BackstagePassesRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.NotSame(item, newItem);
+        }
+
+        [Fact]
+        public void When100000ItemsAreUpdated_ItTakesLessThan200Ms()
+        {
+            var stopwatch = new Stopwatch();
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert" };
+            var sut = new BackstagePassesRuleset();
+
+            stopwatch.Start();
+            for (var i = 0; i < 100000; i++)
+            {
+                sut.UpdateItem(item);
+            }
+            stopwatch.Stop();
+
+            Assert.InRange(stopwatch.ElapsedMilliseconds, 0, 200);
+        }
+    }
+}
