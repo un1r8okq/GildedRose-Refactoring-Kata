43914778b6167faebc5cf89e1e154e2e084adcf3
Will Verland <will@ver.land>
2021-08-01 11:31:15 +1200

    Implement SulfurasRuleset

--- csharpcore/GildedRose/ItemRulesets/SulfurasRuleset.cs
+++ csharpcore/GildedRose/ItemRulesets/SulfurasRuleset.cs
@@ -1,9 +1,13 @@
-﻿using System;
-
-namespace GildedRoseKata
+﻿namespace GildedRoseKata
 {
     public class SulfurasRuleset : ItemRuleset
     {
-        public Item UpdateItem(Item item) => throw new NotImplementedException();
+        public Item UpdateItem(Item item) =>
+            new Item
+            {
+                Name = item.Name,
+                Quality = item.Quality,
+                SellIn = item.SellIn,
+            };
     }
 }
---
+++ csharpcore/GildedRoseTests/ItemRulesets/SulfurasRulesetTests.cs
@@ -0,0 +1,76 @@
+﻿using GildedRoseKata;
+using System.Diagnostics;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class SulfurasRulesetTests
+    {
+        [Theory]
+        [InlineData(10)]
+        [InlineData(1)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void WhenItemIsUpdated_TheSellInValueDoesNotDecrease(int sellIn)
+        {
+            var item = new Item {
+                Name = "Sulfuras, Hand of Ragnaros",
+                SellIn = sellIn,
+            };
+            var sut = new SulfurasRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(sellIn, newItem.SellIn);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(1)]
+        public void WhenSellInIsOne_AndItemIsUpdated_QualityDoesNotDecrease(
+            int quality)
+        {
+            var item = new Item
+            {
+                Name = "Sulfuras, Hand of Ragnaros",
+                Quality = quality,
+                SellIn = 1,
+            };
+            var sut = new SulfurasRuleset();
+
+            var newItem = sut.UpdateItem(item);
+
+            Assert.Equal(quality, item.Quality);
+        }
+
+        [Fact]
+        public void WhenItemIsUpdated_NewObjectIsReturned()
+        {
+            var item = new Item();
+            var sut = new SulfurasRuleset();
+
+            var newItem = sut.UpdateItem(
+                new Item { Name = "Sulfuras, Hand of Ragnaros" });
+
+            Assert.NotSame(item, newItem);
+        }
+
+        [Fact]
+        public void When100000ItemsAreUpdated_ItTakesLessThan200Ms()
+        {
+            var sut = new SulfurasRuleset();
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (var i = 0; i < 100000; i++)
+            {
+                sut.UpdateItem(
+                    new Item { Name = "Sulfuras, Hand of Ragnaros" });
+            }
+            stopwatch.Stop();
+
+            Assert.InRange(stopwatch.ElapsedMilliseconds, 0, 200);
+        }
+    }
+}
