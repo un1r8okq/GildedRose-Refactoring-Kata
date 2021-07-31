f0b7ecb755f7c73de9f16c3764fea2d42b7f418a
Will Verland <will@ver.land>
2021-07-31 20:05:53 +1200

    Add tests for Sulfuras, Hand of Ragnaros

---
+++ csharpcore/GildedRoseTests/SulfurasTests.cs
@@ -0,0 +1,77 @@
+﻿using GildedRoseKata;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class SulfurasTests
+    {
+        [Theory]
+        [InlineData(10)]
+        [InlineData(1)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void WhenOneDayPasses_TheSellInValueDoesNotDecrease(int sellIn)
+        {
+            var items = new List<Item>
+            {
+                new Item {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = sellIn,
+                },
+            };
+            var sut = new GildedRose(items);
+
+            sut.UpdateQuality();
+
+            Assert.Equal(sellIn, items.First().SellIn);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(1)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void WhenTwoDaysPass_TheSellInValueDoesNotDecrease(int sellIn)
+        {
+            var items = new List<Item>
+            {
+                new Item {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = sellIn,
+                },
+            };
+            var sut = new GildedRose(items);
+
+            sut.UpdateQuality();
+            sut.UpdateQuality();
+
+            Assert.Equal(sellIn, items.First().SellIn);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(1)]
+        public void WhenSellInIsOne_AndOneDayPasses_QualityDoesNotDecrease(
+            int quality)
+        {
+            var items = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    Quality = quality,
+                    SellIn = 1,
+                },
+            };
+            var sut = new GildedRose(items);
+
+            sut.UpdateQuality();
+
+            Assert.Equal(quality, items.First().Quality);
+        }
+    }
+}
