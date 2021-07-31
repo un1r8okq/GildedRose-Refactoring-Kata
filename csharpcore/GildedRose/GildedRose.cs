fb8db92aafd78f05eb71648ed63bd2dc0f52ccf4
Will Verland <will@ver.land>
2021-08-01 09:10:58 +1200

    Add constants for item names

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -15,11 +15,11 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != ItemName.AgedBrie && Items[i].Name != ItemName.BackstagePasses)
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Name != ItemName.Sulfuras)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
                         }
@@ -31,7 +31,7 @@ namespace GildedRoseKata
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name == ItemName.BackstagePasses)
                         {
                             if (Items[i].SellIn < 11)
                             {
@@ -52,20 +52,20 @@ namespace GildedRoseKata
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != ItemName.Sulfuras)
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != ItemName.AgedBrie)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name != ItemName.BackstagePasses)
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Name != ItemName.Sulfuras)
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
---
+++ csharpcore/GildedRose/ItemName.cs
@@ -0,0 +1,12 @@
+﻿namespace GildedRoseKata
+{
+    public static class ItemName
+    {
+        public const string AgedBrie =
+            "Aged Brie";
+        public const string BackstagePasses =
+            "Backstage passes to a TAFKAL80ETC concert";
+        public const string Sulfuras =
+            "Sulfuras, Hand of Ragnaros";
+    }
+}
