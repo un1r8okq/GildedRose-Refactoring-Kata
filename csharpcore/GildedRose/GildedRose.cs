593c345a3dda4873fb777179b8aa70350b05187f
Will Verland <will@ver.land>
2021-08-01 12:03:44 +1200

    Refactor GildedRose to use rulesets

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -15,62 +15,8 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (
-                    Items[i].Name != ItemName.AgedBrie &&
-                    Items[i].Name != ItemName.BackstagePasses)
-                {
-                    if (Items[i].Quality > 0 && Items[i].Name != ItemName.Sulfuras)
-                    {
-                       Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == ItemName.BackstagePasses)
-                        {
-                            if (Items[i].SellIn < 11 && Items[i].Quality < 50)
-                            {
-                               Items[i].Quality = Items[i].Quality + 1;
-                            }
-
-                            if (Items[i].SellIn < 6 && Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != ItemName.Sulfuras)
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != ItemName.AgedBrie)
-                    {
-                        if (
-                            Items[i].Name != ItemName.BackstagePasses &&
-                            Items[i].Quality > 0 &&
-                            Items[i].Name != ItemName.Sulfuras)
-                        {
-                           Items[i].Quality = Items[i].Quality - 1;
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
+                var ruleset = ItemRulesetFactory.GetRuleset(Items[i].Name);
+                Items[i] = ruleset.UpdateItem(Items[i]);
             }
         }
     }
